ba73d9d9b7e2d0d11eef6e424cfa0abedafabae8
Tuong Lam <tpi13tla@student.lu.se>
2018-01-25 15:12:41 +0100

    Changed index error in the gen_batch method

--- FirstTests/data_generator.py
+++ FirstTests/data_generator.py
@@ -13,26 +13,35 @@ class data_generator:
         nbr_of_images,dim_squarred = np.shape(images)
         self.trainsize = 10000
         dim = int(np.sqrt(dim_squarred))
-        self.labels = labels[0:self.trainsize-1]
+        self.labels = labels[0:self.trainsize]
         tmp_images = images.reshape((nbr_of_images,dim,dim,1))
         self.images = tmp_images[0:self.trainsize,:,:,:]
         self.ind = 0
         
     def gen_batch(self,batch_size):
+        count = 0
         left = []
         right = []
         sim = []
         
-        if self.ind == 10000:
-            self.ind = 0
-        else:            
-            for i in range(self.ind,self.ind + batch_size + 1):
-                left.append(self.images[i])
-                right.append(self.images[i+1])
-                if self.labels[i] == self.labels[i+1]:
-                    sim.append(1)
-                else:
-                    sim.append(0)
-        self.ind += batch_size
+        while self.ind < self.trainsize - 2:
+            left.append(self.images[self.ind])
+            right.append(self.images[self.ind+1])
+            if self.labels[self.ind] == self.labels[self.ind+1]:
+                sim.append(1)
+            else:
+                sim.append(0)
+            self.ind += 1
+            count += 1
+            
+        for i in range(batch_size - count):
+            left.append(self.images[i])
+            right.append(self.images[i+1])
+            if self.labels[i] == self.labels[i+1]:
+                sim.append(1)
+            else:
+                sim.append(0)
+            self.ind += 1
+            
         return np.array(left),np.array(right),np.array(sim)
         
