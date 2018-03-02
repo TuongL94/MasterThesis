06cdd4afd2351cade0afd6a933f76e27ba5b5920
Exjobb <tpi13tla@student.lu.se>
2018-03-02 11:36:51 +0100

    Implemented an inception module. Made a separate module for utilities for siamese networks, moved training loss,training operations and placeholder initializer to the new module. All models should only include methods related the inference.Lastly some code was refactored.

---
+++ SiameseFingerprint/inception_nn_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 09:57:15 2018
+
+@author: Tuong Lam
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+
+def inception_a_block(input):
+    conv1 = tf.layers.conv2d(
+        inputs = input,
+        filters = 16,
+        kernel_size = [1,1], 
+        padding = "same",
+        activation = tf.nn.relu,
+        reuse = tf.AUTO_REUSE,
+        kernel_initializer = tf.initializers.truncated_normal(mean=0, stddev=0.2),
+        name="conv1_layer_1")
+    
+    conv2 = tf.layers.conv2d(
+        inputs = input,
+        filters = 16,
+        kernel_size = [3,3], 
+        padding = "same",
+        activation = tf.nn.relu,
+        reuse = tf.AUTO_REUSE,
+        kernel_initializer = tf.initializers.truncated_normal(mean=0, stddev=0.2),
+        name="conv2_layer_1")
+    
+    conv3 = tf.layers.conv2d(
+        inputs = input,
+        filters = 16,
+        kernel_size = [5,5], 
+        padding = "same",
+        activation = tf.nn.relu,
+        reuse = tf.AUTO_REUSE,
+        kernel_initializer = tf.initializers.truncated_normal(mean=0, stddev=0.2),
+        name="conv3_layer_1")
+    
+    output = tf.concat([conv1,conv2,conv3],axis=3)
+    return output
+
+def inference(input):
+    output_1 = inception_a_block(input)
+    net = tf.layers.flatten(output_1)
+    return net
